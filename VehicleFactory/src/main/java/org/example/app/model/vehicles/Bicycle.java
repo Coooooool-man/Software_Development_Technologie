29d9e66096696803cb82a361def9e09803d7519f
Coooooool-man <110224419+Coooooool-man@users.noreply.github.com>
2026-03-03 10:56:27 +0600

    Delete project VehicleFactory

--- VehicleFactory/src/main/java/org/example/app/model/vehicles/Bicycle.java
+++
@@ -1,9 +0,0 @@
-package org.example.app.model.vehicles;
-
-import org.example.app.api.Vehicle;
-
-public class Bicycle implements Vehicle {
-    public void drive() {
-        System.out.println("Riding a bicycle.");
-    }
-}
