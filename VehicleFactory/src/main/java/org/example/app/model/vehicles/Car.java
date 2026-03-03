c4826fd94bc80f01942113fc3db013aafa09d692
Andrey Karetnikov <andrew.karetnikov@gmail.com>
2026-03-03 10:59:06 +0600

    Deleted project VehicleFactory

--- VehicleFactory/src/main/java/org/example/app/model/vehicles/Car.java
+++
@@ -1,9 +0,0 @@
-package org.example.app.model.vehicles;
-
-import org.example.app.api.Vehicle;
-
-public class Car implements Vehicle {
-    public void drive() {
-        System.out.println("Diving a car.");
-    }
-}
--- VehicleFactory/src/main/java/org/example/app/model/vehicles/Scooter.java
+++
@@ -1,9 +0,0 @@
-package org.example.app.model.vehicles;
-
-import org.example.app.api.Vehicle;
-
-public class Scooter implements Vehicle {
-    public void drive() {
-        System.out.println("Riding a scotter.");
-    }
-}
